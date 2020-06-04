e1586f3f91db48af2234919793c97e6f78f7d377
Gedihared6409 <gediali019@gmail.com>
2020-06-04 11:59:36 +0300

    initial commit

---
+++ images/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Profile, Image, Comment, Like
+
+# Register your models here.
+
---
+++ images/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Profile, Image, Comment, Like
+from .forms import PostForm, ProfileUploadForm
+
+# Create your views here.
